036e011d180895eaf77fdaa69b51b165492058fb
Ilya Susskiy <susskij@addamant.ru>
2023-03-17 01:29:56 +0600

    убрал log

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const runFile_1 = require("./runFile");
 try {
     (0, runFile_1.run)();
-    console.log("dsa");
 }
 catch (e) {
     console.log(e);
